5f631df8d92b1cb9c5cdb8d622448684b041f9e2
csheha <chanudishehani22@gmail.com>
2024-08-30 11:41:24 +0530

    Password encrypted using bcrypt library

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,10 +1,17 @@
 import BlueJobGiver from "../models/blue-jobgiver.model.js";
+import bcrypt from "bcrypt";
 
 export const signup = async (req,res)=>
 {
     try
     {
-        const newBlueJobGiver = new BlueJobGiver (req.body);
+        const hash = bcrypt.hashSync(req.body.password, 5)
+        const newBlueJobGiver = new BlueJobGiver (
+            {
+                ...req.body,
+                password:hash,
+            }
+        );
 
         await newBlueJobGiver.save();
         res.status(201).send("User has been created!");
